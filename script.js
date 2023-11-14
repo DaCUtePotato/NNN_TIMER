17153897dcdeadb3f31366463811bb76827df929
DaCUtePotato <138492502+DaCUtePotato@users.noreply.github.com>
2023-11-14 11:59:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -41,12 +41,12 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add an event listener to the overlay container
     document.querySelector('.overlay-container').addEventListener('click', function () {
         // Change the background of the site to an image
-        document.body.style.backgroundImage = url("https://github.com/DaCUtePotato/blob/main/background.jpeg");
+        document.body.style.backgroundImage = url("https://raw.githubusercontent.com/DaCUtePotato/main/background.jpeg");
         document.body.style.backgroundSize = 'cover'; // Adjust to your needs
         document.body.style.zIndex = '-1'; // Set a lower z-index for the background
     
         // Play a sound (replace 'sound.mp3' with the path to your sound file)
-        let audio = new Audio('https://github.com/DaCUtePotato/blob/main/sound.mp3');
+        let audio = new Audio('https://raw.githubusercontent.com/DaCUtePotato/main/sound.mp3');
         audio.loop = true
         audio.play();
     });
