e319db868984307b9b0da7c659d2ceacff05533d
DaCUtePotato <138492502+DaCUtePotato@users.noreply.github.com>
2023-11-29 09:06:30 +0100

    script.js Go Nuts button checks the current date and added new sounds

--- script.js
+++ script.js
@@ -7,6 +7,106 @@ document.addEventListener('DOMContentLoaded', function () {
     let isBackgroundOn = false; // Initial state: background is off
     let backgroundChangeInterval; // Variable to store the interval ID
     let countdown = 2000; // Initial countdown value
+    let isCountdownClicked = false;
+
+    function startBackgroundChangeInterval() {
+        // Clear the previous interval, if any
+        clearInterval(backgroundChangeInterval);
+
+        // Set a new interval with a callback to changeBackground with the current countdown value
+        backgroundChangeInterval = setInterval(function () {
+            // play transition sound
+            playTransitionSound();
+            changeBackground(0.2);
+
+            if (isCountdownClicked) {
+                // Add any additional logic here
+            }
+        }, countdown);
+    }
+
+    document.querySelector('.countdown-container').onclick = function () {
+        // Toggle the background change variable
+        isBackgroundOn = !isBackgroundOn;
+
+        // Toggle the isCountdownClicked variable
+        isCountdownClicked = !isCountdownClicked;
+
+        // play transition sound
+        playTransitionSound();
+
+        // Change the background immediately
+        changeBackground();
+
+        if (isBackgroundOn && isCountdownClicked) {
+            // Clear the previous interval, if any
+            clearInterval(backgroundChangeInterval);
+
+            // Set a new interval with a callback to changeBackground with the current countdown value
+            backgroundChangeInterval = setInterval(function () {
+                // play transition sound
+                playTransitionSound();
+                changeBackground(0.2);
+            }, countdown);
+
+            // Start the countdown immediately after the click
+            updateCountdown();
+        } else {
+            // If the background is off, clear the interval
+            clearInterval(backgroundChangeInterval);
+        }
+    };
+
+    function changeBackground() {
+        // Generate a random number from 1 to 7 for the image path
+        let randomImageNumber = Math.floor(Math.random() * 7) + 1;
+
+        // Construct the image path
+        let imagePath = `img/Hintergrund${randomImageNumber}.jpg`;
+
+        // Set the body background image
+        document.body.style.backgroundImage = `url('${imagePath}')`;
+
+        // Log the image path for debugging
+        console.log('Changed background to:', imagePath);
+    }
+
+    function playTransitionSound(volume=0.2) {
+        // Create an Audio element
+        const audio = new Audio('sounds/transition.mp3');
+
+        audio.volume = volume;
+
+        // Play the audio
+        audio.play();
+    }
+
+    function updateCountdown() {
+        let now = new Date();
+        let timeDifference = Math.abs(targetDate - now);
+        let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        let milliseconds = timeDifference % 1000;
+
+        // Format milliseconds to always have three digits
+        milliseconds = milliseconds.toString().padStart(3, '0');
+
+        document.getElementById('countdown').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s ${milliseconds}ms`;
+
+        // Request the next animation frame
+        requestAnimationFrame(updateCountdown);
+    }
+
+    // Initial call to start the animation
+    requestAnimationFrame(updateCountdown);
+
+    // Define currentDate before using it in the if condition
+    let currentDate = new Date();
+    if (currentDate.getMonth() === 10 && currentDate.getDate() === 29) {
+        addGoNutsButton(); // Call the function to add the Go Nuts button
+    }
 
     function addGoNutsButton() {
         const existingButton = document.getElementById('goNutsButton');
@@ -30,58 +130,4 @@ document.addEventListener('DOMContentLoaded', function () {
             document.body.appendChild(container);
         }
     }
-
-    addGoNutsButton(); // Call the function to add the Go Nuts button
-
-    document.querySelector('.countdown-container').onclick = function () {
-        // Toggle the background change variable
-        isBackgroundOn = !isBackgroundOn;
-
-        // Change the background immediately
-        changeBackground();
-
-        if (isBackgroundOn) {
-            // Clear the previous interval, if any
-            clearInterval(backgroundChangeInterval);
-
-            // Log just before setting the interval
-            console.log("Setting up the interval...");
-
-            // Set a new interval with a callback to changeBackground with the current countdown value
-            backgroundChangeInterval = setInterval(function () {
-                console.log("Inside setInterval"); // Check if this log is printed
-                changeBackground();
-                console.log("The image should have changed by now…");
-            }, countdown);
-        } else {
-            // If the background is off, clear the interval
-            clearInterval(backgroundChangeInterval);
-        }
-    };
-
-    function changeBackground() {
-        // Generate a random number from 1 to 7 for the image path
-        let randomImageNumber = Math.floor(Math.random() * 7) + 1;
-
-        // Construct the image path
-        let imagePath = `img/Hintergrund${randomImageNumber}.jpg`;
-
-        // Set the body background image
-        document.body.style.backgroundImage = `url('${imagePath}')`;
-
-        // Log the image path for debugging
-        console.log('Changed background to:', imagePath);
-    }
-
-    setInterval(function () {
-        let now = new Date();
-        let timeDifference = Math.abs(targetDate - now);
-        let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
-        let milliseconds = timeDifference % 1000;
-
-        document.getElementById('countdown').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s ${milliseconds}ms`;
-    });
 });
